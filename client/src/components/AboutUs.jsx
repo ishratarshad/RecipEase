aad9b7ab23ef06ef34969dddb200a7dad53b910e
Angela Yu <angelayu0530@gmail.com>
2025-06-23 16:39:25 -0400

    basic aboutus page

---
+++ client/src/components/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AboutUs = () => {
+    return (
+        <div style={{ padding: '40px', fontFamily: 'Poppins, sans-serif', backgroundColor: '#fef6f9' }}>
+        <h1 style={{ fontSize: '2.5rem', color: '#ff80ab' }}>About Us</h1>
+        <p style={{ fontSize: '1.2rem', color: '#555', maxWidth: '600px', margin: '20px auto' }}>
+            At RecipEase, we are passionate about making meal planning accessible and enjoyable for everyone, regardless of dietary restrictions. Our mission is to provide personalized meal solutions that cater to your unique needs, ensuring that you can enjoy delicious and safe meals every day.
+        </p>
+        <p style={{ fontSize: '1.2rem', color: '#555', maxWidth: '600px', margin: '20px auto' }}>
+            The team members are Ishrat Arshad, Angela Yu, Bonnie Lei, Shithila Urshi
+        </p>
+        </div>
+    );
+    }
+export default AboutUs;
